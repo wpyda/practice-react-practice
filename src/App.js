d6d85831ae1c45939026c8a7666e33448865827c
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-12 09:24:46 +0100

    first component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+// import React, {Component} from 'react';
+// import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+//
+// import MyFirstComponent from './MyFirstComponent'
+// import Dashboard from './Dashboard'
+// import Home from './Home'
+//
+// class App extends Component {
+//     render() {
+//         return (
+//         <Router>
+//             <div>
+//                 <Link to="/">Link to main</Link><br/>
+//                 <Link to="/home">Link to home</Link><br/>
+//                 <Link to="/dashboard">Link to dashboard</Link><br/>
+//
+//                 <Route path="/" exact={true} render={
+//                     () => (<MyFirstComponent name="Mateusz" showName={true} />)
+//                 }/>
+//                 <Route path="/home" component={Home} />
+//                 <Route path="/dashboard" component={Dashboard} />
+//
+//             </div>
+//         </Router>
+//         )
+//     }
+// }
+//
+// export default App;
+//
+//
+import React from 'react'
 
-import MyFirstComponent from './MyFirstComponent'
-import Dashboard from './Dashboard'
-import Home from './Home'
-
-class App extends Component {
-    render() {
-        return (
-        <Router>
-            <div>
-                <Link to="/">Link to main</Link><br/>
-                <Link to="/home">Link to home</Link><br/>
-                <Link to="/dashboard">Link to dashboard</Link><br/>
-
-                <Route path="/" exact={true} render={
-                    () => (<MyFirstComponent name="Mateusz" showName={true} />)
-                }/>
-                <Route path="/home" component={Home} />
-                <Route path="/dashboard" component={Dashboard} />
-
-            </div>
-        </Router>
-        )
-    }
+const App = () => {
+    return <div>App</div>
 }
 
-export default App;
-
-
-
-
-
-
-
-
+export default App
